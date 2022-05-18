274b80a460876eb317464b3e7126d1c9b42a6317
MysterJacob <kubapoloszyk@gmail.com>
2022-05-18 21:37:18 +0200

    Dodaj pliki projektów.

---
+++ HexColor/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace HexColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.color_chooser = new System.Windows.Forms.PictureBox();
+            this.R_Color = new System.Windows.Forms.TrackBar();
+            this.G_Color = new System.Windows.Forms.TrackBar();
+            this.B_Color = new System.Windows.Forms.TrackBar();
+            this.R_Color_Box = new System.Windows.Forms.TextBox();
+            this.G_Color_Box = new System.Windows.Forms.TextBox();
+            this.B_Color_Box = new System.Windows.Forms.TextBox();
+            this.RGB_Value = new System.Windows.Forms.TextBox();
+            this.Hex_Value = new System.Windows.Forms.TextBox();
+            this.CMYK_Value = new System.Windows.Forms.TextBox();
+            this.HSV_Color = new System.Windows.Forms.TrackBar();
+            this.HSV_Value = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.color_chooser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.R_Color)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.G_Color)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Color)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HSV_Color)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // color_chooser
+            // 
+            this.color_chooser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.color_chooser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.color_chooser.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.color_chooser.Location = new System.Drawing.Point(12, 12);
+            this.color_chooser.Name = "color_chooser";
+            this.color_chooser.Size = new System.Drawing.Size(350, 324);
+            this.color_chooser.TabIndex = 0;
+            this.color_chooser.TabStop = false;
+            this.color_chooser.MouseDown += new System.Windows.Forms.MouseEventHandler(this.color_chooser_MouseDown);
+            this.color_chooser.MouseLeave += new System.EventHandler(this.color_chooser_MouseLeave);
+            this.color_chooser.MouseUp += new System.Windows.Forms.MouseEventHandler(this.color_chooser_MouseUp);
+            // 
+            // R_Color
+            // 
+            this.R_Color.Location = new System.Drawing.Point(368, 12);
+            this.R_Color.Maximum = 255;
+            this.R_Color.Name = "R_Color";
+            this.R_Color.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.R_Color.Size = new System.Drawing.Size(45, 324);
+            this.R_Color.SmallChange = 0;
+            this.R_Color.TabIndex = 1;
+            this.R_Color.TickFrequency = 8;
+            this.R_Color.Scroll += new System.EventHandler(this.R_color_Scroll);
+            // 
+            // G_Color
+            // 
+            this.G_Color.Location = new System.Drawing.Point(419, 12);
+            this.G_Color.Maximum = 255;
+            this.G_Color.Name = "G_Color";
+            this.G_Color.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.G_Color.Size = new System.Drawing.Size(45, 324);
+            this.G_Color.SmallChange = 0;
+            this.G_Color.TabIndex = 2;
+            this.G_Color.TickFrequency = 8;
+            this.G_Color.Scroll += new System.EventHandler(this.G_color_Scroll);
+            // 
+            // B_Color
+            // 
+            this.B_Color.Location = new System.Drawing.Point(470, 12);
+            this.B_Color.Maximum = 255;
+            this.B_Color.Name = "B_Color";
+            this.B_Color.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.B_Color.Size = new System.Drawing.Size(45, 324);
+            this.B_Color.SmallChange = 0;
+            this.B_Color.TabIndex = 3;
+            this.B_Color.TickFrequency = 8;
+            this.B_Color.Scroll += new System.EventHandler(this.B_Color_Scroll);
+            // 
+            // R_Color_Box
+            // 
+            this.R_Color_Box.Location = new System.Drawing.Point(364, 342);
+            this.R_Color_Box.Name = "R_Color_Box";
+            this.R_Color_Box.Size = new System.Drawing.Size(49, 20);
+            this.R_Color_Box.TabIndex = 4;
+            this.R_Color_Box.Text = "0";
+            this.R_Color_Box.TextChanged += new System.EventHandler(this.R_Color_Box_TextChanged);
+            // 
+            // G_Color_Box
+            // 
+            this.G_Color_Box.Location = new System.Drawing.Point(415, 342);
+            this.G_Color_Box.Name = "G_Color_Box";
+            this.G_Color_Box.Size = new System.Drawing.Size(49, 20);
+            this.G_Color_Box.TabIndex = 5;
+            this.G_Color_Box.Text = "0";
+            this.G_Color_Box.TextChanged += new System.EventHandler(this.G_Color_Box_TextChanged);
+            // 
+            // B_Color_Box
+            // 
+            this.B_Color_Box.Location = new System.Drawing.Point(466, 342);
+            this.B_Color_Box.MaxLength = 3;
+            this.B_Color_Box.Multiline = true;
+            this.B_Color_Box.Name = "B_Color_Box";
+            this.B_Color_Box.Size = new System.Drawing.Size(49, 20);
+            this.B_Color_Box.TabIndex = 6;
+            this.B_Color_Box.Text = "0";
+            this.B_Color_Box.TextChanged += new System.EventHandler(this.B_Color_Box_TextChanged);
+            // 
+            // RGB_Value
+            // 
+            this.RGB_Value.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RGB_Value.Location = new System.Drawing.Point(521, 24);
+            this.RGB_Value.Name = "RGB_Value";
+            this.RGB_Value.ReadOnly = true;
+            this.RGB_Value.Size = new System.Drawing.Size(157, 27);
+            this.RGB_Value.TabIndex = 7;
+            this.RGB_Value.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Hex_Value
+            // 
+            this.Hex_Value.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Hex_Value.Location = new System.Drawing.Point(521, 50);
+            this.Hex_Value.Name = "Hex_Value";
+            this.Hex_Value.ReadOnly = true;
+            this.Hex_Value.Size = new System.Drawing.Size(157, 27);
+            this.Hex_Value.TabIndex = 8;
+            this.Hex_Value.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // CMYK_Value
+            // 
+            this.CMYK_Value.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CMYK_Value.Location = new System.Drawing.Point(521, 76);
+            this.CMYK_Value.Name = "CMYK_Value";
+            this.CMYK_Value.ReadOnly = true;
+            this.CMYK_Value.Size = new System.Drawing.Size(157, 27);
+            this.CMYK_Value.TabIndex = 9;
+            this.CMYK_Value.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // HSV_Color
+            // 
+            this.HSV_Color.Location = new System.Drawing.Point(12, 342);
+            this.HSV_Color.Maximum = 360;
+            this.HSV_Color.Name = "HSV_Color";
+            this.HSV_Color.Size = new System.Drawing.Size(350, 45);
+            this.HSV_Color.SmallChange = 0;
+            this.HSV_Color.TabIndex = 11;
+            this.HSV_Color.Scroll += new System.EventHandler(this.HSV_Color_Scroll);
+            // 
+            // HSV_Value
+            // 
+            this.HSV_Value.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HSV_Value.Location = new System.Drawing.Point(521, 100);
+            this.HSV_Value.Name = "HSV_Value";
+            this.HSV_Value.ReadOnly = true;
+            this.HSV_Value.Size = new System.Drawing.Size(157, 27);
+            this.HSV_Value.TabIndex = 12;
+            this.HSV_Value.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.HSV_Value.TextChanged += new System.EventHandler(this.HSV_Value_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 377);
+            this.Controls.Add(this.HSV_Value);
+            this.Controls.Add(this.HSV_Color);
+            this.Controls.Add(this.CMYK_Value);
+            this.Controls.Add(this.Hex_Value);
+            this.Controls.Add(this.RGB_Value);
+            this.Controls.Add(this.B_Color_Box);
+            this.Controls.Add(this.G_Color_Box);
+            this.Controls.Add(this.R_Color_Box);
+            this.Controls.Add(this.B_Color);
+            this.Controls.Add(this.G_Color);
+            this.Controls.Add(this.R_Color);
+            this.Controls.Add(this.color_chooser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.color_chooser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.R_Color)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.G_Color)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B_Color)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HSV_Color)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox color_chooser;
+        private System.Windows.Forms.TrackBar R_Color;
+        private System.Windows.Forms.TrackBar G_Color;
+        private System.Windows.Forms.TrackBar B_Color;
+        private System.Windows.Forms.TextBox R_Color_Box;
+        private System.Windows.Forms.TextBox G_Color_Box;
+        private System.Windows.Forms.TextBox B_Color_Box;
+        private System.Windows.Forms.TextBox RGB_Value;
+        private System.Windows.Forms.TextBox Hex_Value;
+        private System.Windows.Forms.TextBox CMYK_Value;
+        private System.Windows.Forms.TrackBar HSV_Color;
+        private System.Windows.Forms.TextBox HSV_Value;
+    }
+}
+
---
+++ HexColor/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace HexColor
+{
+    public partial class Form1 : Form
+    {
+        int r, g, b;
+        Bitmap color_chooser_bitmap;
+        bool mouse_pressed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //[DllImport("kernel32.dll")]
+        //public static extern bool AllocConsole();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            color_chooser_bitmap = new Bitmap(128, 128);
+            update();
+            Thread chooser_thread = new Thread(new ThreadStart(color_select_thread));
+            chooser_thread.Start();
+        }
+        private void update() 
+        {
+            Color color = Color.FromArgb(r, g, b);
+            R_Color_Box.Text = r.ToString();
+            G_Color_Box.Text = g.ToString();
+            B_Color_Box.Text = b.ToString();
+
+            R_Color.Value = r;
+            G_Color.Value = g;
+            B_Color.Value = b;
+
+            RGB_Value.Text = $"{r},{g},{b}";
+            Hex_Value.Text = $"#{r.ToString("X2")}{g.ToString("X2")}{b.ToString("X2")}";
+
+            BackColor = color;
+
+            float r_p = r / 255.0f;
+            float g_p = g / 255.0f;
+            float b_p = b / 255.0f;
+
+            double k = 1.0f - Math.Max(Math.Max(r_p, g_p), b_p);
+            double c = k == 1 ? 0 : ( (1.0 - r_p - k) / (1.0 - k)  );
+            double m = k == 1 ? 0 : ( (1.0 - g_p - k) / (1.0 - k) );
+            double y = k == 1 ? 0 : ( (1.0 - b_p - k) / (1.0 - k) );
+
+            int h = (int)color.GetHue();
+            int s = (int)(color.GetSaturation()*100);
+            int v = (int)(color.GetBrightness()*100);
+
+            HSV_Value.Text = $"{h}°,{s}%,{v}%";
+
+
+            c = Math.Round(c * 100.0f);
+            m = Math.Round(m * 100.0f);
+            y= Math.Round(y * 100.0f);
+            k = Math.Round(k * 100.0f);
+
+            CMYK_Value.Text = $"{c},{m},{y},{k}";
+
+            UpdateColorBox(color);
+        }
+        private void update(Color color)
+        {
+            r = color.R;
+            g = color.G;
+            b = color.B;
+            update();
+        }
+        private void color_select_thread()
+        {
+            while (!Disposing)
+            {
+                if (mouse_pressed)
+                {
+                    float relative_x = Cursor.Position.X - Left - color_chooser.Location.X;
+                    float relative_y = Cursor.Position.Y - Top - color_chooser.Location.Y;
+
+                    int x = (int) ((relative_x / (float)color_chooser.Width) * color_chooser_bitmap.Width);
+                    int y = (int) ((relative_y / (float)color_chooser.Height) * color_chooser_bitmap.Height);
+
+                    x = Math.Min(color_chooser_bitmap.Width - 1, Math.Max(0, x));
+                    y = Math.Min(color_chooser_bitmap.Height - 1, Math.Max(0, y));
+
+                    Color new_color = color_chooser_bitmap.GetPixel(x, y);
+
+                    //HSV_Value.Invoke((Action)delegate { HSV_Value.Text = $"{x}:{y} {bitmap.Width}:{bitmap.Height}"; });
+                    Invoke((Action)delegate { update(new_color); });
+                }
+            }
+        }
+        private Color lerpColorRGB(Color c1,Color c2,float t)
+        {
+            return Color.FromArgb(
+                (int) (c1.R * (1 - t) + c2.R * t),
+                (int) (c1.G * (1 - t) + c2.G * t),
+                (int) (c1.B * (1 - t) + c2.B * t)
+            );
+        }
+        private Color HueToRGB(float hue,float saturation,float value)
+        {
+//https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+            float c = value * saturation;
+            float x = c * (1.0f - Math.Abs((hue / 60.0f) % 2.0f - 1.0f));
+
+            float[] rgb_prim = new float[] { 0, 0, 0 };
+            //rgb_prim = { 23,31,3};
+            if (hue < 60)
+                rgb_prim = new float[] { c, x, 0 };
+            else if (hue < 120)
+                rgb_prim = new float[] { x, c, 0 };
+            else if (hue < 180)
+                rgb_prim = new float[] { 0, c, x };
+            else if (hue < 240)
+                rgb_prim = new float[] { 0, x, c };
+            else if (hue < 300)
+                rgb_prim = new float[] { x, 0, c };
+            else if (hue < 360)
+                rgb_prim = new float[] { c, 0, x };
+
+            float m = value - c;
+
+            int r = (int)((rgb_prim[0] + m) * 255.0f);
+            int g = (int)((rgb_prim[1] + m) * 255.0f);
+            int b = (int)((rgb_prim[2] + m) * 255.0f);
+            return Color.FromArgb(r, g, b);
+        }
+        private void UpdateColorBox(Color color)
+        {
+            float hue = color.GetHue();
+            color = HueToRGB(hue, 1, 1);
+
+            for(int x = 0;x < 128; x++)
+            {
+                for(int y = 0; y < 128; y++)
+                {
+                    float xt = x / 128.0f;
+                    float yt = y / 128.0f;
+
+                    Color saturated = lerpColorRGB(color, Color.White, 1 - xt);
+                    Color blacken = lerpColorRGB(saturated, Color.Black, yt);
+
+                    color_chooser_bitmap.SetPixel(x, y, blacken);
+                }
+            }
+            color_chooser.Image = color_chooser_bitmap;
+            color_chooser.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Update();
+            R_Color.Update();
+            B_Color.Update();
+            G_Color.Update();
+        }
+        private int parse_color_box(string value)
+        {
+            int result = 0;
+            if(int.TryParse(value,out result))
+            {
+                result = Math.Min(Math.Max(0, result), 255);
+            }
+            return result;
+        }
+        private void R_Color_Box_TextChanged(object sender, EventArgs e)
+        {
+            int parsed = parse_color_box(R_Color_Box.Text);
+            r = parsed;
+            update();
+        }
+
+        private void R_color_Scroll(object sender, EventArgs e)
+        {
+            r = R_Color.Value;
+            update();
+        }
+
+        private void color_chooser_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_pressed = true;
+        }
+
+        private void color_chooser_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_pressed = false;
+        }
+
+        private void color_chooser_MouseLeave(object sender, EventArgs e)
+        {
+            mouse_pressed = false;
+        }
+
+        private void HSV_Value_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HSV_Color_Scroll(object sender, EventArgs e)
+        {
+            Color color = HueToRGB(HSV_Color.Value, 1, 1);
+            update(color);
+        }
+
+        private void G_color_Scroll(object sender, EventArgs e)
+        {
+            g = G_Color.Value;
+            update();
+        }
+
+        private void B_Color_Scroll(object sender, EventArgs e)
+        {
+            b = B_Color.Value;
+            update();
+        }
+
+        private void G_Color_Box_TextChanged(object sender, EventArgs e)
+        {
+            int parsed = parse_color_box(G_Color_Box.Text);
+            g = parsed;
+            update();
+        }
+
+        private void B_Color_Box_TextChanged(object sender, EventArgs e)
+        {
+            int parsed = parse_color_box(B_Color_Box.Text);
+            b = parsed;
+            update();
+        }
+    }
+}
